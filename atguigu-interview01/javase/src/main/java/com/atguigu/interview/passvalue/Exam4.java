e14a6276ea29165bc87d88a5809a0e15141a384c
miaoqi <miaoqi3@xdf.cn>
2023-05-13 17:03:29 +0800

    update blog

--- atguigu-interview01/javase/src/main/java/com/atguigu/interview/passvalue/Exam4.java
+++ atguigu-interview01/javase/src/main/java/com/atguigu/interview/passvalue/Exam4.java
@@ -2,6 +2,16 @@ package com.atguigu.interview.passvalue;
 
 import java.util.Arrays;
 
+/**
+ * 形参是基本数据类型
+ * * 传递数据值
+ * 实参是引用数据类型
+ * * 传递地址值
+ * * 特殊的类型：String、包装类等对象不可变性, 会在内存中产生新的对象
+ *
+ * @author miaoqi
+ * @date 2023-05-12 17:33:2
+ */
 public class Exam4 {
 
     public static void main(String[] args) {
