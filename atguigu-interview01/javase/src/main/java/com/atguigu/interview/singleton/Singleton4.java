0f025ef9d16bb4b842272fcd87fb68a729f5a47b
miaoqi <miaoqi3@xdf.cn>
2023-11-14 17:56:21 +0800

    update blog

--- atguigu-interview01/javase/src/main/java/com/atguigu/interview/singleton/Singleton4.java
+++ atguigu-interview01/javase/src/main/java/com/atguigu/interview/singleton/Singleton4.java
@@ -1,12 +1,12 @@
 package com.atguigu.interview.singleton;
 
 /*
- * 懒汉式：
+ * 懒汉式: 
  * 	延迟创建这个实例对象
  *
  * (1)构造器私有化
  * (2)用一个静态变量保存这个唯一的实例
- * (3)提供一个静态方法，获取这个实例对象
+ * (3)提供一个静态方法, 获取这个实例对象
  */
 public class Singleton4 {
 
--- atguigu-interview01/javase/src/main/java/com/atguigu/interview/step/TestStep.java
+++ atguigu-interview01/javase/src/main/java/com/atguigu/interview/step/TestStep.java
@@ -3,7 +3,7 @@ package com.atguigu.interview.step;
 import org.junit.Test;
 
 /**
- * 编程题：有n步台阶，一次只能上1步或2步，共有多少种走法？
+ * 编程题: 有n步台阶, 一次只能上1步或2步, 共有多少种走法？
  *
  * @author miaoqi
  * @date 2023-05-13 15:58:28
@@ -18,7 +18,7 @@ public class TestStep {
         System.out.println(end - start);//586ms
     }
 
-    //实现f(n)：求n步台阶，一共有几种走法
+    //实现f(n): 求n步台阶, 一共有几种走法
     public int f(int n) {
         if (n < 1) {
             throw new IllegalArgumentException(n + "不能小于1");
